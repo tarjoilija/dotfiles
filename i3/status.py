6fe9a077c301c9ee1d95bc16a290da46eba08933
tarjoilija <eero.saisa@awly.org>
2015-11-06 09:02:23 +0200

    i3: refactor i3pystatus configuration

--- i3/status.py
+++ i3/status.py
@@ -1,92 +1,91 @@
 #!/usr/bin/env python3
 
 import subprocess
-
 from i3pystatus import Status
-
 status = Status(standalone=True)
 
-status.register("clock",
-    format="%a %-d %b %X",
-    color="#6fb3d2")
+COLOR_BLUE    = "#64b3d2"
+COLOR_YELLOW  = "#fda331"
+COLOR_RED     = "#fb0120"
+COLOR_GREEN   = "#a1c659"
 
+# {{{ CLOCK
+status.register("clock",
+    format="%a %-d %b %T",
+    color=COLOR_BLUE)
+
+# }}}
+
+# {{{ RESOURCES
 status.register("mem",
-    color="#fda331",
-    warn_color="#fda331",
-    alert_color="#fb0120",
-    format="mem {percent_used_mem:.0f}%")
+    format="mem {percent_used_mem:.0f}%",
+    color=COLOR_GREEN,
+    warn_color=COLOR_YELLOW,
+    alert_color=COLOR_RED)
 
 status.register("load",
-    format="cpu {avg1} {avg5}",
-    color="#a1c659",
-    critical_color="#fb0120")
+    format="cpu {avg1}",
+    color=COLOR_GREEN,
+    critical_color=COLOR_RED)
 
+status.register("temp",
+    format="temp {temp:.0f}°C",
+    color=COLOR_GREEN,
+    alert_temp=60,
+    alert_color=COLOR_RED)
+# }}}
+
+# {{{ BATTERY
 status.register("battery",
-    format="{status}/{percentage_design:.0f}% {consumption:.2f}W {remaining:%E%hh:%Mm}",
+    format="bat {status}/{percentage_design:.0f}% {remaining:%E%h:%M}",
     battery_ident="BAT1",
     alert=True,
-    alert_percentage=5,
+    alert_percentage=15,
     not_present_text="",
     status={
         "DIS": "↓",
         "CHR": "↑",
         "FULL": "=",
     },
-    full_color="#6fb3d2",
-    charging_color="#6fb3d2",
-    critical_color="#fb0120",
-    color="#fda331")
+    full_color=COLOR_GREEN,
+    charging_color=COLOR_BLUE,
+    critical_color=COLOR_RED,
+    color=COLOR_YELLOW)
+# }}}
 
-status.register("network_traffic",
-    interface="usb0",
-    hide_down=True,
-    format="d{bytes_recv:4.0f} u{bytes_sent:4.0f}",)
+# {{{ NETWORK INTERFACES
+for i in ["wlan0", "usb0", "eth0", "eth1"]:
+    status.register("network",
+        interface=i,
+        format_up="{interface} {v4cidr}",
+        color_up=COLOR_GREEN,
+        format_down="")
 
-status.register("network_traffic",
-    interface="eth1",
-    hide_down=True,
-    format="d{bytes_recv:4.0f} u{bytes_sent:4.0f}",)
+# }}}
 
-status.register("network_traffic",
-    interface="wlan0",
-    hide_down=True,
-    format="d{bytes_recv:4.0f} u{bytes_sent:4.0f}",)
-
+# {{{ WIFI
 status.register("network",
-    interface="usb0",
-    format_up="{interface} {v4}",
-    format_down="",
-    color_up="#a1c659",
-    unknown_up=True)
-
-status.register("network",
-    interface="eth1",
-    format_up="{interface} {v4}",
-    format_down="",
-    color_up="#a1c659",
-    unknown_up=True)
-
-status.register("wireless",
-    color_up="#a1c659",
     interface="wlan0",
-    format_down="",
-    format_up="{essid} {quality:.0f}%",)
+    format_up="{essid} {quality}%",
+    color_up=COLOR_BLUE,
+    format_down="")
+# }}}
 
-status.register("bitcoin",
-    color="#be643c",
-    format="btc {last_price:.0f}")
-
+# {{{ DISK USAGE
 status.register("disk",
     path="/",
-    color="#76c7b7",
+    color=COLOR_YELLOW,
     format="{avail}G",)
+# }}}
 
+# {{{ AUDIO
 status.register("pulseaudio",
     format="♪{volume}",
-    color_unmuted="#a1c659",
-    color_muted="#fb0120")
+    color_unmuted=COLOR_GREEN,
+    color_muted=COLOR_RED)
 
 status.register("now_playing",
-    color="#fda331")
+    color=COLOR_YELLOW)
+# }}}
 
 status.run()
